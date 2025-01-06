7b29781c5dc102239551f2a7cbf97ffa6d390ff5
Trần Hải <102784614+trnghai@users.noreply.github.com>
2025-01-07 00:13:15 +0700

    Update step.py

--- 07.01.25/step.py
+++ 07.01.25/step.py
@@ -1,3 +1,5 @@
+ans = []
 for _ in range(int(input())):
     n = input()
-    print("YES" if all(n[i] <= n[i+1] for i in range(len(n)-1)) and len(n) > 1 else "NO")
+    ans.append("YES" if all(n[i] <= n[i+1] for i in range(len(n)-1)) and len(n) > 1 else "NO")
+print("\n".join(ans))
